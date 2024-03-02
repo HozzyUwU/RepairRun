e1d7b97ffd03e4485e7de968f31e0ed8f1178a8c
Andrey <naumaika1999@gmail.com>
2024-03-02 19:23:36 +0200

    Added enemy object along with navmesh agent and navmesh surface for environment and basic enemyAI script

---
+++ Assets/Scripts/enemyai.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyai : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    UnityEngine.AI.NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
