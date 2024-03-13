659421edc4871c8bb5128fa5504e7a8f70216906
Andrey <naumaika1999@gmail.com>
2024-03-13 17:17:49 +0200

    Test

--- Assets/Scripts/enemyai.cs
+++ Assets/Scripts/enemyai.cs
@@ -6,7 +6,9 @@ using UnityEngine.AI;
 public class enemyai : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
     UnityEngine.AI.NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,17 @@ public class enemyai : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceToTarget <= chaseRange)
+        {
         navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGismos()
+    {
+        // Display the chase range of the enemy
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
